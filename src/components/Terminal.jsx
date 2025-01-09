8747cefc924757c91a2d8c3282ce14e68a2067c1
AndrewBlevins <andrew.s.blevins@gmail.com>
2025-01-09 15:19:06 -0500

    Make welcome text more helpful

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -2678,10 +2678,10 @@ ${selectedText}
 
   useEffect(() => {
     setMessages([
-      { type: 'system', content: 'SPACE Terminal - v.0.1' },
-      { type: 'system', content: `Context limit (length up to which full conversation memory is retained) is set to ${contextLimit.toLocaleString()} tokens. Reduce it to save money.`},
+      { type: 'system', content: 'Welcome to SPACE - v.0.1' },
+      { type: 'system', content: `Context limit (length up to which full conversation memory is retained) is set to ${contextLimit.toLocaleString()} tokens. (Reduce it to save money.)`},
       { type: 'system', content: `Max length for responses is set to ${maxTokens.toLocaleString()} tokens.`},
-      { type: 'system', content: `Type /help for a list of commands.` }
+      { type: 'system', content: `Type /help for a list of commands. Type /prompt list to see a list of available starting prompts. Press + (to the left of here) to add an advisor to the board.` }
     ]);
   }, []);
 
