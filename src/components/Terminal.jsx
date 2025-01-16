8ad0d6f0841020e069aaeff444e3489438fbedc9
AndrewBlevins <andrew.s.blevins@gmail.com>
2025-01-16 09:52:48 -0800

    Add info button

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -3073,6 +3073,29 @@ ${selectedText}
           )}
         </div>
       )}
+      {/* Add this near the top where other buttons are rendered */}
+      <div className="fixed bottom-4 right-4 z-50">
+        <a
+          href="https://github.com/andrewblevins/space"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center w-8 h-8 rounded-full bg-black border border-green-400 text-green-400 hover:bg-green-400 hover:text-black transition-colors"
+          title="About SPACE Terminal"
+        >
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="h-5 w-5" 
+            viewBox="0 0 20 20" 
+            fill="currentColor"
+          >
+            <path 
+              fillRule="evenodd" 
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" 
+              clipRule="evenodd" 
+            />
+          </svg>
+        </a>
+      </div>
     </>
   );
 }
